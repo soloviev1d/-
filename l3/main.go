93049b7a89dbff294a04863975c9d0226912ff40
soloviev1d <soloviev.dm11@gmail.com>
2024-10-10 16:23:55 +0300

    DOC lab №3

--- l3/main.go
+++ l3/main.go
@@ -10,7 +10,11 @@ import (
 )
 
 func main() {
+	// create a scanner for stdin
 	scn := bufio.NewReader(os.Stdin)
+
+	// define a Mealy finite state machine with initial state 0 | -
+	// and state changes described by FSM.StateMap
 	fsm := FSM.NewFSM(FSM.StateOutputTuple{"0", "-"}, FSM.StateMap{
 		"0": FSM.StateTransition{
 			"50":  FSM.StateOutputTuple{"50", "-"},
@@ -39,18 +43,25 @@ func main() {
 		},
 	})
 
+	// transition state until output signal is equal to something other than "-"
 	for fsm.Output() == "-" {
+		// print prompt and read user input
 		fmt.Print("> ")
 		l, err := scn.ReadString('\n')
 		if err != nil {
 			log.Fatalln("Failed to read string", err)
 		}
+
+		// trim '\n'
 		l = l[:len(l)-1]
 
+		// check if user entered a valid value according to possible transitions
 		if _, ok := fsm.StateMap[fsm.Current()][FSM.Event(l)]; !ok {
 			fmt.Println("Invalid input:", l, "State was not changed")
 			continue
 		}
+
+		// transition state
 		fsm.Transition(FSM.Event(l))
 		fmt.Println("Inserted:", fsm.Current())
 	}
